2d0dfc0aecf80e7011a2447de1f78d5474b9f0e3
Quinntyne Brown <quinntynebrown@gmail.com>
2016-12-31 10:09:29 -0500

    notifications

---
+++ PingedNotificationHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+
+namespace MediatRGettingStarted
+{
+    public class PingedNotificationHandler : INotificationHandler<Pinged>
+    {
+        public void Handle(Pinged notification)
+        {
+            Console.WriteLine("Notified");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,22 +22,15 @@ namespace MediatRGettingStarted
     {
         public static async Task Run(IMediator mediator, TextWriter writer)
         {
-            await writer.WriteLineAsync("Sample mediator implementation using send, publish and post-request handlers in sync and async version.");
+            await writer.WriteLineAsync("Sample mediator implementation");
             await writer.WriteLineAsync("---------------");
 
             await writer.WriteLineAsync("Sending Ping...");
             var pong = await mediator.SendAsync(new Ping { Message = "Ping" });
+
+            mediator.Publish(new Pinged());
+
             await writer.WriteLineAsync("Received: " + pong.Message);
-
-            //await writer.WriteLineAsync("Sending Ping async...");
-            //var response = await mediator.Send(new PingAsync { Message = "Ping" });
-            //await writer.WriteLineAsync("Received async: " + response.Message);
-
-            //await writer.WriteLineAsync("Publishing Pinged...");
-            //await mediator.Publish(new Pinged());
-
-            //await writer.WriteLineAsync("Publishing Pinged async...");
-            //await mediator.Publish(new PingedAsync());
         }
     }
 }
