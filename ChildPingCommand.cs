7b4f8a1bd6eeda95d1095bb426cee5200682321f
Quinntyne Brown <quinntynebrown@gmail.com>
2017-02-17 01:33:02 -0500

    remove extra lines

--- ChildPingCommand.cs
+++ ChildPingCommand.cs
@@ -8,36 +8,17 @@ namespace MediatRGettingStarted
 {
     public class ChildPingCommand
     {
-        public class ChildPingRequest : IRequest<ChildPingResponse>
-        {
-            public ChildPingRequest()
-            {
+        public class ChildPingRequest : IRequest<ChildPingResponse> { }
 
-            }
-        }
-
-        public class ChildPingResponse
-        {
-            public ChildPingResponse()
-            {
-
-            }
-        }
+        public class ChildPingResponse { }
 
         public class ChildPingHandler : IAsyncRequestHandler<ChildPingRequest, ChildPingResponse>
         {
-            public ChildPingHandler()
-            {
-            }
-
             public async Task<ChildPingResponse> Handle(ChildPingRequest request)
             {
                 Console.WriteLine("Child Ping");
-                return new ChildPingResponse();
-            }
-            
+                return await Task.FromResult(new ChildPingResponse());
+            }            
         }
-
     }
-
 }
